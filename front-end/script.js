a9e97036a0e101cecc68d2559e525c5ece6bebde
JuniorEsbaltar <junior.esbaltar@gmail.com>
2021-01-26 16:27:27 -0300

    form

---
+++ front-end/script.js
@@ -0,0 +1,15 @@
+// const headerName = document.getElementById('title')
+
+// const colors = ['#02feff','#0000fd','#fe0000','#ff7f00','#ffff01','#00ff01']
+
+// const changeHeaderNameColor = (i) => {
+//   i == colors.length - 1 ? changeHeaderNameColor(0) : changeColor(i)
+// }
+
+
+// const changeColor = (i) => {
+//   headerName.style.color = colors[i]
+
+//   setTimeout(()=>{changeHeaderNameColor(i+1)},200)
+// }
+// changeHeaderNameColor(0)
